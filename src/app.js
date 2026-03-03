d68eaf8f091b8c5d00c0af9975763682b05e48c5
iaroslav_marchuk <jarik.13a@gmail.com>
2026-03-03 20:29:11 +0000

    add work

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: allowedOrigin, credentials: true }));
-app.options('*', cors({ origin: allowedOrigin, credentials: true }));
+// app.options('*', cors({ origin: allowedOrigin, credentials: true }));
 
 app.use(cookieParser());
 
